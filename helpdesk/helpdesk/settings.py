cea2e8daa42b9b78e0b08df6411ff99b2bbb5e10
adi19git <dyadityakumar@gmail.com>
2026-05-14 00:34:14 +0530

    fix: remove duplicate DB config, move migrate to Procfile

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'helpdesk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=False,
     )
 }
 
