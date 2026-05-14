d885e1c857ffbe06cd2985584b696e9ad63de96d
adi19git <dyadityakumar@gmail.com>
2026-05-14 23:08:38 +0530

    fix postgres ssl

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=False,
+        ssl_require=True,
     )
 }
 
